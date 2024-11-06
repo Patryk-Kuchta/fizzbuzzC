8a549ec42ffca7f9c608b3f40b326c848d7b8601
Patryk-Kuchta <patryk.kuchta@softwire.com>
2024-11-06 14:02:25 +0000

    rudimentary approach

--- Program.cs
+++ Program.cs
@@ -4,7 +4,27 @@
     {
         for (int index = start; index <= end; index++)
         {
-            Console.WriteLine(index);
+            Console.WriteLine(responseToNumber(index));
+        }
+    }
+
+    static string responseToNumber(int number)
+    {
+        if (number % 15 == 0)
+        {
+            return "FizzBuzz";
+        }
+        else if (number % 5 == 0) 
+        {
+            return "Buzz";
+        }
+        else if (number % 3 == 0)
+        {
+            return "Fizz";
+        }
+        else
+        {
+            return number.ToString();
         }
     }
 
