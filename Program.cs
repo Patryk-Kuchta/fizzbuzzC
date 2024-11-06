e03d0851595d105e6f8496c23464b9cdb89b0b2a
Patryk-Kuchta <patryk.kuchta@softwire.com>
2024-11-06 13:56:56 +0000

    simple integer loop

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
 ﻿class FizzBuzz
 {
+    static void mapOverIntegerRange(int start, int end)
+    {
+        for (int index = start; index <= end; index++)
+        {
+            Console.WriteLine(index);
+        }
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int start = 1; // inclusive
+        int end = 100; // inclusive
+
+        FizzBuzz.mapOverIntegerRange(start, end);
     }
 }
-
