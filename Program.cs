8bb883814bc435b9cb319c5cf8bd8b40649a095b
Patryk-Kuchta <patryk.kuchta@softwire.com>
2024-11-06 14:46:00 +0000

    convoluted way of approaching part_1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿class FizzBuzz
+﻿using System.Collections.Generic;
+
+class FizzBuzz
 {
     private static void mapOverIntegerRange(int startInclusive, int endInclusive, Action<int> action)
     {
@@ -15,22 +17,29 @@
 
     private static void responseToNumber(int number)
     {
-        if (isDivisibleBy(number, 3) && isDivisibleBy(number, 5))
+        var divisorsAndNames = new Dictionary<int, string>
         {
-            Console.WriteLine("FizzBuzz");
-        }
-        else if (isDivisibleBy(number, 5)) 
-        {
-            Console.WriteLine("Buzz");
-        }
-        else if (isDivisibleBy(number, 3))
-        {
-            Console.WriteLine("Fizz");
-        }
+            { 3, "Fizz" },
+            { 5, "Buzz" },
+
+        };
+
+        List<string> answerComponents = new List<string> {};
+
+        foreach (var divisor in divisorsAndNames.Keys)
+            if (isDivisibleBy(number, divisor)) {
+                answerComponents.Add(divisorsAndNames[divisor]);
+            }
+
+        if (answerComponents.Count > 0) 
+            Console.WriteLine(composeString(answerComponents.ToArray()));
         else
-        {
             Console.WriteLine(number.ToString());
-        }
+    }
+
+    private static string composeString(string[] list)
+    {
+        return String.Join("", list);
     }
 
     public static void Main(string[] args)
