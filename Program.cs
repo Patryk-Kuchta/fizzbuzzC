ec623e3d72db281c028c20e27da343ef361d7872
Patryk-Kuchta <patryk.kuchta@softwire.com>
2024-11-08 09:37:12 +0000

    remove the redundant second divisorAndNames[divisor]

--- Program.cs
+++ Program.cs
@@ -45,7 +45,7 @@ class FizzBuzz
             if (isDivisibleBy(number, divisor))
             {
                 var action = divisorsAndNames[divisor];
-                answerComponents.Add(divisorsAndNames[divisor]);
+                answerComponents.Add(action);
                 if (action != CompositionActions.ReverseAll)
                     showNumber = false;
             }
