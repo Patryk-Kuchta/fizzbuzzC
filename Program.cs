6d62bfbbea5ff8309b38e74a06d052fc16156f6a
Patryk-Kuchta <patryk.kuchta@softwire.com>
2024-11-08 16:48:18 +0000

    make a start on writting the new class structure

--- Program.cs
+++ Program.cs
@@ -1,159 +1,151 @@
 ﻿using System.Diagnostics;
 
-class FizzBuzz
+namespace FizzBuzzNamespace
 {
-    private static void mapOverIntegerRange(int startInclusive, int endInclusive, Action<int> action)
+    class FizzBuzz
     {
-        for (int index = startInclusive; index <= endInclusive; index++)
+        private static void mapOverIntegerRange(int startInclusive, int endInclusive, Action<int> action)
         {
-            action(index);
-        }
-    }
-
-    private static bool isDivisibleBy(int number, int divisior)
-    {
-        return number % divisior == 0;
-    }
-
-    // order of the action matters, they must be applied in this order
-    private enum CompositionActions
-    {
-        AppendFizz,
-        AppendBuzz,
-        AppendBang,
-        ReplaceEverythingWithBong,
-        SuffixWithFezz,
-        ReverseAll
-    }
-
-    private static string responseToNumber(int number)
-    {
-        var divisorsAndNames = new Dictionary<int, CompositionActions>
-        {
-            { 3, CompositionActions.AppendFizz },
-            { 5, CompositionActions.AppendBuzz },
-            { 7, CompositionActions.AppendBang },
-            { 11, CompositionActions.ReplaceEverythingWithBong },
-            { 13, CompositionActions.SuffixWithFezz },
-            { 17, CompositionActions.ReverseAll }
-        };
-
-        var answerComponents = new List<CompositionActions> { };
-        bool showNumber = true;
-
-        foreach (var divisor in divisorsAndNames.Keys)
-            if (isDivisibleBy(number, divisor))
+            for (int index = startInclusive; index <= endInclusive; index++)
             {
-                var action = divisorsAndNames[divisor];
-                answerComponents.Add(action);
-                if (action != CompositionActions.ReverseAll)
-                    showNumber = false;
+                action(index);
             }
+        }
 
-        if (showNumber)
-            return number.ToString();
-        else
-            return composeString(answerComponents.ToArray());
-    }
-
-    private static string composeString(CompositionActions[] list)
-    {
-        var output = new List<string>();
-        int? indexOfFirstB = null;
-
-        foreach (var action in list)
+        private static bool isDivisibleBy(int number, int divisior)
         {
-            switch (action)
-            {
-                case CompositionActions.AppendFizz:
-                    {
-                        output.Add("Fizz");
-                        break;
-                    }
-                case CompositionActions.AppendBuzz:
-                case CompositionActions.AppendBang:
-                    {
-                        string name = action.ToString().Replace("Append", "");
+            return number % divisior == 0;
+        }
 
-                        if (!indexOfFirstB.HasValue)
+        private static string responseToNumber(int number)
+        {
+            var divisorsAndNames = new Dictionary<int, CompositionActions>
+            {
+                { 3, CompositionActions.AppendFizz },
+                { 5, CompositionActions.AppendBuzz },
+                { 7, CompositionActions.AppendBang },
+                { 11, CompositionActions.ReplaceEverythingWithBong },
+                { 13, CompositionActions.SuffixWithFezz },
+                { 17, CompositionActions.ReverseAll }
+            };
+
+            var answerComponents = new List<CompositionActions> { };
+            bool showNumber = true;
+
+            foreach (var divisor in divisorsAndNames.Keys)
+                if (isDivisibleBy(number, divisor))
+                {
+                    var action = divisorsAndNames[divisor];
+                    answerComponents.Add(action);
+                    if (action != CompositionActions.ReverseAll)
+                        showNumber = false;
+                }
+
+            if (showNumber)
+                return number.ToString();
+            else
+                return composeString(answerComponents.ToArray());
+        }
+
+        private static string composeString(CompositionActions[] list)
+        {
+            var output = new List<string>();
+            int? indexOfFirstB = null;
+
+            foreach (var action in list)
+            {
+                switch (action)
+                {
+                    case CompositionActions.AppendFizz:
                         {
-                            indexOfFirstB = output.Count;
+                            output.Add("Fizz");
+                            break;
                         }
+                    case CompositionActions.AppendBuzz:
+                    case CompositionActions.AppendBang:
+                        {
+                            string name = action.ToString().Replace("Append", "");
 
-                        output.Add(name);
-                        break;
-                    }
-                case CompositionActions.ReplaceEverythingWithBong:
-                    {
-                        output = new List<string> { "Bong" };
-                        indexOfFirstB = 0;
-                        break;
-                    }
-                case CompositionActions.SuffixWithFezz:
-                    {
-                        int insertFezzAtIndex = indexOfFirstB ?? output.Count;
+                            if (!indexOfFirstB.HasValue)
+                            {
+                                indexOfFirstB = output.Count;
+                            }
 
-                        output.Insert(insertFezzAtIndex, "Fezz");
-                        break;
-                    }
-                case CompositionActions.ReverseAll:
-                    {
-                        output.Reverse();
-                        break;
-                    }
+                            output.Add(name);
+                            break;
+                        }
+                    case CompositionActions.ReplaceEverythingWithBong:
+                        {
+                            output = new List<string> { "Bong" };
+                            indexOfFirstB = 0;
+                            break;
+                        }
+                    case CompositionActions.SuffixWithFezz:
+                        {
+                            int insertFezzAtIndex = indexOfFirstB ?? output.Count;
+
+                            output.Insert(insertFezzAtIndex, "Fezz");
+                            break;
+                        }
+                    case CompositionActions.ReverseAll:
+                        {
+                            output.Reverse();
+                            break;
+                        }
+                }
+            }
+            return String.Join("", output.ToArray());
+        }
+
+        public static void testResponseToNumber()
+        {
+            var testCases = new Dictionary<int, string> {
+                { 3, "Fizz" },
+                { 5, "Buzz" },
+                { 7, "Bang" },
+                { 11, "Bong" },
+                { 13, "Fezz" },
+                { 3 * 5, "FizzBuzz" },
+                { 3 * 7, "FizzBang" },
+                { 5 * 7, "BuzzBang" },
+                { 3 * 5 * 7, "FizzBuzzBang" },
+                { 11 * 3, "Bong" },
+                { 11 * 5, "Bong" },
+                { 11 * 7, "Bong" },
+                { 11 * 13, "FezzBong" },
+                { 13 * 3, "FizzFezz" },
+                { 13 * 5, "FezzBuzz" },
+                { 13 * 7, "FezzBang" },
+                { 13 * 3 * 5, "FizzFezzBuzz" },
+                { 13 * 3 * 5 * 7, "FizzFezzBuzzBang" },
+                { 3 * 5 * 17, "BuzzFizz" },
+                { 5 * 17, "Buzz" },
+                { 3 * 7 * 17, "BangFizz" },
+                { 7 * 11 * 13 * 17, "BongFezz" },
+                { 1, "1" },
+                { 2, "2" },
+                { 4, "4" },
+                { 8, "8" },
+                { 17, "17" }
+            };
+
+            foreach (var input in testCases.Keys)
+            {
+                var output = responseToNumber(input);
+
+                Debug.Assert(output == testCases[input],
+                    "testResponseToNumber answered '" + output + "', but was expected to answer '" + testCases[input] + "' to input = " + input.ToString());
             }
         }
-        return String.Join("", output.ToArray());
-    }
 
-    public static void testResponseToNumber()
-    {
-        var testCases = new Dictionary<int, string> {
-            { 3, "Fizz" },
-            { 5, "Buzz" },
-            { 7, "Bang" },
-            { 11, "Bong" },
-            { 13, "Fezz" },
-            { 3 * 5, "FizzBuzz" },
-            { 3 * 7, "FizzBang" },
-            { 5 * 7, "BuzzBang" },
-            { 3 * 5 * 7, "FizzBuzzBang" },
-            { 11 * 3, "Bong" },
-            { 11 * 5, "Bong" },
-            { 11 * 7, "Bong" },
-            { 11 * 13, "FezzBong" },
-            { 13 * 3, "FizzFezz" },
-            { 13 * 5, "FezzBuzz" },
-            { 13 * 7, "FezzBang" },
-            { 13 * 3 * 5, "FizzFezzBuzz" },
-            { 13 * 3 * 5 * 7, "FizzFezzBuzzBang" },
-            { 3 * 5 * 17, "BuzzFizz" },
-            { 5 * 17, "Buzz" },
-            { 3 * 7 * 17, "BangFizz" },
-            { 7 * 11 * 13 * 17, "BongFezz" },
-            { 1, "1" },
-            { 2, "2" },
-            { 4, "4" },
-            { 8, "8" },
-            { 17, "17" }
-        };
-
-        foreach (var input in testCases.Keys)
+        public static void Main(string[] args)
         {
-            var output = responseToNumber(input);
+            int startInclusive = 1;
+            int endInclusive = 100;
 
-            Debug.Assert(output == testCases[input],
-                "testResponseToNumber answered '" + output + "', but was expected to answer '" + testCases[input] + "' to input = " + input.ToString());
+            testResponseToNumber();
+
+            mapOverIntegerRange(startInclusive, endInclusive, (input) => Console.WriteLine(responseToNumber(input)));
         }
-    }
-
-    public static void Main(string[] args)
-    {
-        int startInclusive = 1;
-        int endInclusive = 100;
-
-        testResponseToNumber();
-
-        mapOverIntegerRange(startInclusive, endInclusive, (input) => Console.WriteLine(responseToNumber(input)));
     }
 }
---
+++ SimpleCompositionAction.cs
@@ -0,0 +1,56 @@
+﻿using FizzBuzzNamespace;
+using System.Reflection.Emit;
+
+//{ 3, CompositionActions.AppendFizz },
+//                { 5, CompositionActions.AppendBuzz },
+//                { 7, CompositionActions.AppendBang },
+//                { 11, CompositionActions.ReplaceEverythingWithBong },
+//                { 13, CompositionActions.SuffixWithFezz },
+//                { 17, CompositionActions.ReverseAll }
+
+namespace FizzBuzzNamespace
+{
+    // order of the action matters, they must be applied in this order
+    public enum CompositionActions
+    {
+        AppendFizz,
+        AppendBuzz,
+        AppendBang,
+        ReplaceEverythingWithBong,
+        SuffixWithFezz,
+        ReverseAll
+    }
+
+    public class SimpleCompositionAction
+    {
+        private int DivisorExpected;
+        protected string Label;
+
+        public SimpleCompositionAction(int divisorExpected, string label)
+        {
+            DivisorExpected = divisorExpected;
+            Label = label;
+        }
+
+        public virtual List<string> apply(List<string> currentLabelSet)
+        {
+            currentLabelSet.Add(Label);
+            return currentLabelSet;
+        }
+
+        public bool isMeetingActionCondition(int input)
+        {
+            return input % DivisorExpected == 0;
+        }
+    }
+
+    public class ReplacerCompositionAction : SimpleCompositionAction
+    {
+        public ReplacerCompositionAction(int divisorExpected, string label): base(divisorExpected, label) {}
+
+        public override List<string> apply(List<string> currentLabelSet)
+        {
+            return new List<string> { base.Label };
+        }
+    }
+}
